22cd95e56f2324d795075ea1c8271f005439480f
beataj <beata@emc2wave.com>
2021-04-29 22:43:02 -0700

    arrays annotation or inference

---
+++ arrays.ts
@@ -0,0 +1,10 @@
+const carMakers = ['ford', 'toyota', 'chevy'];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+    ['Focus', 'f15'],
+    ['cororla'],
+    ['suny', '23r']
+];
+
+const tracksByMake: string[][] = [];
