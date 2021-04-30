eb5a881015d29200e64aa0f2b54d608b1cd42512
beataj <beata@emc2wave.com>
2021-04-29 22:53:22 -0700

    typescript help ...with arrays method

--- arrays.ts
+++ arrays.ts
@@ -8,3 +8,15 @@ const carsByMake = [
 ];
 
 const tracksByMake: string[][] = [];
+
+// Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop()
+
+// Prevent incompatible values
+carMakers.push(100);
+
+// Help with 'map'
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+})
