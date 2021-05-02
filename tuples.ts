1fed2707eec46ea5386825724485f13ffa39f5b8
beataj <beata@emc2wave.com>
2021-05-01 17:48:24 -0700

    create type Drink

--- tuples.ts
+++ tuples.ts
@@ -4,4 +4,8 @@ const drink = {
     suger: 40
 }
 
-const pepsi: [string, boolean, number] = ['brown', true, 40]
+type Drink = [string, boolean, number]
+
+const pepsi: Drink = ['brown', true, 40];
+const spring: Drink = ['clear', true, 40];
+const tea: Drink = ['brown', false, 0]
