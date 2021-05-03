02b4d25e4513eae892c831a81e84b0822c964f39
beataj <beata@emc2wave.com>
2021-05-02 20:43:31 -0700

    add summary() to interface

--- interfaces.ts
+++ interfaces.ts
@@ -1,13 +1,17 @@
 interface Vehicle {
     name: string;
     year: Date;
-    broken: boolean
+    broken: boolean;
+    summary():string;
 }
 
 const oldCivic = {
     name: 'civic',
     year: new Date(),
-    broken: true
+    broken: true,
+    summary():string {
+        return `Name: ${this.name}`
+    }
 }
 
 
