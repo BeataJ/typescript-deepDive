315bc33bd16aabfc8b767b2f05c6dea755a8ba30
beataj <beata@emc2wave.com>
2021-05-03 17:12:32 -0700

    add private modifier

--- classes.ts
+++ classes.ts
@@ -1,16 +1,17 @@
 class Vehicle {
-    public drive(): void {
-        console.log('chugga chugga')
-    }
-
+    
     public honk():void {
         console.log('beep')
     }
 }
 
 class Car extends Vehicle {
-    public drive():void {
+    private drive():void {
         console.log('vroom')
+    }
+
+    startDrivingProcess():void {
+        this.drive()
     }
 }
 
@@ -18,5 +19,5 @@ class Car extends Vehicle {
 // vehicle.drive();
 // vehicle.honk();
 const car = new Car();
-car.drive();
+car.startDrivingProcess();
 car.honk();
