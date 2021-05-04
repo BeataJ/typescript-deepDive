c979d67aba35401f5739a4d7ebff6717a06387fe
beataj <beata@emc2wave.com>
2021-05-04 14:33:26 -0700

    add agrument color red

--- classes.ts
+++ classes.ts
@@ -10,21 +10,21 @@ class Vehicle {
     }
 }
 
-// class Car extends Vehicle {
-//     private drive():void {
-//         console.log('vroom')
-//     }
+class Car extends Vehicle {
+    private drive():void {
+        console.log('vroom')
+    }
 
-//     startDrivingProcess():void {
-//         this.drive();
-//         this.honk()
-//     }
-// }
+    startDrivingProcess():void {
+        this.drive();
+        this.honk()
+    }
+}
 
 const vehicle = new Vehicle('orange');
-// // vehicle.drive();
-// // vehicle.honk();
+// vehicle.drive();
+// vehicle.honk();
 console.log(vehicle.color)
-// const car = new Car();
-// car.startDrivingProcess();
+const car = new Car('red');
+car.startDrivingProcess();
 // car.honk();
