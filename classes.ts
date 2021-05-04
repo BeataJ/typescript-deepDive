ad763095be1935977e18a8672211b69e9f2d0646
beataj <beata@emc2wave.com>
2021-05-03 17:30:39 -0700

    add color property

--- classes.ts
+++ classes.ts
@@ -1,24 +1,30 @@
 class Vehicle {
+    color: string;
+
+    constructor(color: string) {
+        this.color = color;
+    }
     
     protected honk():void {
         console.log('beep')
     }
 }
 
-class Car extends Vehicle {
-    private drive():void {
-        console.log('vroom')
-    }
+// class Car extends Vehicle {
+//     private drive():void {
+//         console.log('vroom')
+//     }
 
-    startDrivingProcess():void {
-        this.drive();
-        this.honk()
-    }
-}
+//     startDrivingProcess():void {
+//         this.drive();
+//         this.honk()
+//     }
+// }
 
-// const vehicle = new Vehicle();
-// vehicle.drive();
-// vehicle.honk();
-const car = new Car();
-car.startDrivingProcess();
+const vehicle = new Vehicle('orange');
+// // vehicle.drive();
+// // vehicle.honk();
+console.log(vehicle.color)
+// const car = new Car();
+// car.startDrivingProcess();
 // car.honk();
